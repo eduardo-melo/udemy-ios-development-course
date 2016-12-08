c88b1993b151ec91b0c6bfb579d846f95deebffa
Eduardo <eduardomelo29@gmail.com>
2016-12-08 16:10:21 -0300

    Finish lecture 27

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        let ageInCatYears = Int(textField.text!)! * 7;
+        result.text = String(ageInCatYears)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
        
     }
 
-    @IBAction func buttonTapped(_ sender: Any) {
-       
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
